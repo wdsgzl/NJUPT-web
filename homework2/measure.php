7a05dd01a26c8d13d4153013773d028a3fc007da
Arox <39874451+wdsgzl@users.noreply.github.com>
2024-05-27 09:12:50 +0800

    Add files via upload

---
+++ homework2/measure.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<?php echo Your name is;
+      echo $_GET["username"]; <br>
+echo Your choice is;
+for($i=0;$i<count(%_post[]);$i++){
+echo $_POST["light"][$i]."$nbsp;";
+}
+echo Your approache to pay is; 
+echo $_POST["radio"]; ?>
+
+</body>
+</html>
